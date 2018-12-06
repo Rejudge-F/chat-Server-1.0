f262297f5764842f380c40ea199aaed14965df02
Rejudge-F <44461064+Rejudge-F@users.noreply.github.com>
2018-12-06 21:35:19 +0800

    更新代码
    修正已经知道的错误代码

--- client.cpp
+++ client.cpp
@@ -1,4 +1,3 @@
-
 //
 //  main.cpp
 //  Server
@@ -15,40 +14,48 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string>
 
 #define BACKLOG 20
 #define MAX_CON_NO 10
-#define MAX_DATA_SIZE 1024
+#define MAX_DATA_SIZE 4096
 
-int main(int argc, char * argv[]) {
+int main(int argc,char *argv[]) {
     if(argc != 3) {
-        puts("Usage: ./client <IP> <PORT>");
-        exit(0);
+        puts("Usage: ./Client <IP> <PORT>");
+        exit(1);
     }
+
     int client_sock = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(atoi(argv[2]));
     server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    connect(client_sock, (sockaddr *) & server_addr, sizeof(server_addr));
     if(client_sock < 0) {
-        perror("init client");
-        exit(1);
-    }
-    
-    puts("success to init client...");
-    
-    if(connect(client_sock, (sockaddr *) & server_addr, sizeof(server_addr)) < 0) {
         perror("connect");
         exit(1);
     }
-    
-    puts("success connect server...");
-    char sendBuff[MAX_DATA_SIZE];
-    while(true) {
-        fgets(sendBuff, MAX_DATA_SIZE, stdin);
-        write(client_sock, sendBuff, strlen(sendBuff));
+    char send_buff[MAX_DATA_SIZE], recv_buff[MAX_DATA_SIZE];
+    int pid = fork();
+    if(pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if(pid > 0) {
+        while(true) {
+            fgets(send_buff, MAX_DATA_SIZE, stdin);
+            //printf("#Client: %s\n", send_buff);
+            send(client_sock, send_buff, strlen(send_buff), 0);
+            memset(send_buff, 0, sizeof(send_buff));
+        }
+    } else {
+        while(true) {
+            recv(client_sock, recv_buff, MAX_DATA_SIZE, 0);
+            printf("#Server: %s", recv_buff);
+            memset(recv_buff, 0, sizeof(recv_buff));
+        }
     }
+    close(client_sock);
     return 0;
 }
