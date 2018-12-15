fa74f7b38d5d7f5d27c7469f586fd1201dcbd572
Rejudge-F <44461064+Rejudge-F@users.noreply.github.com>
2018-12-15 20:37:32 +0800

    Update Message.cpp

--- header/Message/Message.cpp
+++ header/Message/Message.cpp
@@ -1,15 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-class Message {
-public:
-    Message ();
-    ~Message();
-    bool setMessage(std::string _message);
-    char * getMessage();
-    int getAddress();
-    void clear();
-private:
-    char address[1024];
-    char message[1024];
-};
+#include "Message.h"
+#include <cstring>
+
+Message::Message() {
+    memset(address, 0, sizeof(address));
+    memset(message, 0, sizeof(message));
+}
+
+Message::~Message() {
+}
+
+bool Message::setMessage(std::string _message) {
+    int len = _message.length();
+    for(int i = len - 2; i >= 0; i--) {
+        if(_message[i] == '-' && _message[i + 1] == '>') {
+            strcpy(message, _message.substr(0, i).c_str());
+            strcpy(address, _message.substr(i + 2).c_str());
+            break;
+        }
+    }
+    len = strlen(address);
+    address[len - 1] = '\0';
+    if(len == 0) return false;
+    for(int i = 0; i < len - 1; i++) { //len-1 for '\n'
+        if(!isdigit(address[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+char * Message::getMessage() {
+    char * ptr = message;
+    return ptr;
+}
+
+int Message::getAddress() {
+    return atoi(address);
+}
+
+void Message::clear() {
+    memset(address, 0, sizeof(address));
+    memset(message, 0, sizeof(message));
+}
